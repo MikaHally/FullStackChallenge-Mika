67b7ac3e3247d6115724f536610fed659ac517db
Mika Hally <mika.hally@code.berlin>
2018-12-24 14:09:36 +0100

    Setted up SQL server and connected FE and BE with node.js on localhost

--- src/mailForm.js
+++ src/mailForm.js
@@ -2,26 +2,84 @@ import React from "react";
 import "./mailForm.css";
 
 export default class MailForm extends React.Component {
-  submitMail() {
-    alert("Your e-mail has been sent.");
+  state = {
+    mails: [],
+    mail: {
+      recipient: "mika.hally",
+      subject: "Xmas",
+      content: "Hello everyone"
+    }
+  };
+
+  /*
+  componentDidMount() {
+    this.getInformation();
   }
-  reset() {
+
+  getInformation = () => {
+    fetch("http://localhost:5000/products")
+      .then(response => response.json())
+      .then(({ data }) => {
+        console.log(data);
+      })
+      .catch(err => console.error(err));
+  };
+  */
+
+  // Sending out request
+  sendMail = () => {
+    const { mail } = this.state;
+    fetch(
+      `http://localhost:5000/add?recipient=${mail.recipient}&subject=${
+        mail.subject
+      }&content=${mail.content}`
+    )
+      .then(response => response.json())
+      .then(this.getInformation)
+      .catch(err => console.error(err));
+  };
+  // Resetting all input values to undefined
+  reset = function() {
     document.getElementById("mail-inputs").reset();
-  }
+  };
   render() {
+    const { mails, mail } = this.state;
     return (
       <div className="mail-form">
         <div className="navbar_wrapper">
           <p>Neue Nachricht</p>
         </div>
-        <form id="mail-inputs">
-          <input placeholder="An" className="input" type="email" required />
-          <input placeholder="Betreff" className="input" />
-          <textarea className="textarea" required />
-          <button className="submit-button" onClick={this.submitMail}>
+        <div id="mail-inputs">
+          <input
+            placeholder="An"
+            className="input"
+            type="email"
+            required
+            value={mail.recipient}
+            onChange={e =>
+              this.setState({ mail: { ...mail, recipient: e.target.value } })
+            }
+          />
+          <input
+            placeholder="Betreff"
+            className="input"
+            value={mail.subject}
+            onChange={e =>
+              this.setState({ mail: { ...mail, subject: e.target.value } })
+            }
+          />
+          <textarea
+            className="textarea"
+            required
+            value={mail.content}
+            onChange={e =>
+              this.setState({ mail: { ...mail, content: e.target.value } })
+            }
+          />
+          <button className="submit-button" onClick={this.sendMail}>
             Senden
           </button>
-        </form>
+        </div>
         <div className="attachement">
           <div className="image-upload">
             <label for="file-input">
